77546f086165af2766298cc442204f00694159aa
Zhulmukhametov <tipoyapro@gmail.com>
2018-10-27 23:20:14 +0500

    db

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -6,21 +6,23 @@ DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
 
+CREATE TABLE user_roles
+(
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  roles_name   TEXT NOT NULL,
+  CONSTRAINT user_roles_idx UNIQUE (roles_name)
+);
+
 CREATE TABLE users
 (
   id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   login TEXT NOT NULL,
   name TEXT NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  role_id INTEGER NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES user_roles(id) ON DELETE CASCADE
 );
 
-CREATE TABLE user_roles
-(
-  user_id INTEGER NOT NULL,
-  roles    TEXT PRIMARY KEY,
-  CONSTRAINT user_roles_idx UNIQUE (user_id, roles),
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
 
 CREATE TABLE tickets
 (
@@ -35,10 +37,10 @@ CREATE TABLE tickets
 
 CREATE TABLE sites
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   domain_name TEXT NOT NULL,
-  user_id INTEGER,
-  roles_name TEXT,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-  FOREIGN KEY (roles_name) REFERENCES user_roles (roles) ON DELETE CASCADE
-);
+  user_id     INTEGER,
+  role_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (role_id) REFERENCES user_roles (id)
+)
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -3,30 +3,31 @@ DELETE FROM users;
 DELETE FROM tickets;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (login, name, password) VALUES
+INSERT INTO user_roles (roles_name) VALUES
+  ('Admin'),
+  ('User');
 
-  ('User', 'Ivan', 'password'),
-  ('Admin', 'Andrey', 'password');
+INSERT INTO users (login, name, password, role_id) VALUES
 
-INSERT INTO user_roles (roles, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+  ('User', 'Ivan', 'password',100000),
+  ('Admin', 'Andrey', 'password',100001);
 
-INSERT INTO sites (domain_name, user_id, roles_name) VALUES
-  ('vk.com', null , 'ROLE_USER'),
-  ('github.com',null,'ROLE_ADMIN'),
-  ('google.com',null,'ROLE_USER'),
-  ('yandex.ru',null,'ROLE_USER'),
-  ('youtube.com',null,'ROLE_ADMIN');
+
+INSERT INTO sites (domain_name, user_id, role_id) VALUES
+  ('vk.com', null , 100000),
+  ('github.com',null,100001),
+  ('google.com',null,100000),
+  ('google.com',null,100001),
+  ('youtube.com',100003,100000);
 
 
 INSERT INTO tickets (user_id, date_time, description, site_id) VALUES
-  (100000, '2015-05-30 10:00:00', 'Example', 100003),
-  (100000, '2015-05-30 13:00:00', 'Test', 100004),
-  (100000, '2015-05-30 15:00:00', 'Test', 100005),
-  (100000, '2015-05-30 17:00:00', 'Test', 100006),
-  (100000, '2015-05-31 12:00:00', 'Test', 100003),
-  (100000, '2015-05-31 15:00:00', 'Test', 100006),
-  (100000, '2015-06-29 12:00:00', 'Test', 100004),
-  (100000, '2015-06-30 18:00:00', 'Test', 100006);
+  (100002, '2015-05-30 10:00:00', 'Example', 100004),
+  (100003, '2015-05-30 13:00:00', 'Test', 100004),
+  (100002, '2015-05-30 15:00:00', 'Test', 100005),
+  (100003, '2015-05-30 17:00:00', 'Test', 100006),
+  (100003, '2015-05-31 12:00:00', 'Test', 100007),
+  (100002, '2015-05-31 15:00:00', 'Test', 100006),
+  (100003, '2015-06-29 12:00:00', 'Test', 100004),
+  (100002, '2015-06-30 18:00:00', 'Test', 100006);
 
