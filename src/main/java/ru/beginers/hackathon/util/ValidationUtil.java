e4d8add1d7e5987f52763d33cbd7cac7d9e03b9e
Ilya Shikhovtsev <shikhovtsev97@gmail.com>
2018-10-27 17:53:04 +0500

    added logback

---
+++ src/main/java/ru/beginers/hackathon/util/ValidationUtil.java
@@ -0,0 +1,43 @@
+package ru.beginers.hackathon.util;
+
+import ru.beginers.hackathon.model.AbstractBaseEntity;
+import ru.beginers.hackathon.util.exception.NotFoundException;
+
+public class ValidationUtil {
+
+    private ValidationUtil() {
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        return checkNotFound(object, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(AbstractBaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalArgumentException(entity + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(AbstractBaseEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.getId() != id) {
+            throw new IllegalArgumentException(entity + " must be with id=" + id);
+        }
+    }
+}
