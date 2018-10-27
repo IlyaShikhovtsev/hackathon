24669a635543d5a8b67297bffa00df775e9ba57a
shikhovtsevDS <mikro192@mail.ru>
2018-10-27 14:21:13 +0500

    Changes

--- src/main/java/ru/beginers/hackathon/model/AbstractNamedEntity.java
+++ src/main/java/ru/beginers/hackathon/model/AbstractNamedEntity.java
@@ -1,17 +1,7 @@
 package ru.beginers.hackathon.model;
 
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
-
-@MappedSuperclass
 public abstract class AbstractNamedEntity extends AbstractBaseEntity {
 
-    @NotBlank
-    @Size(min = 2, max = 100)
-    @Column(name = "name", nullable = false)
     protected String name;
 
     public AbstractNamedEntity() {
