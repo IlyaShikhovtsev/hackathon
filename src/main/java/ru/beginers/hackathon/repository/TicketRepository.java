a50fb0f0a95e113fda93502c0b76724074b61edc
Ilya Shikhovtsev <shikhovtsev97@gmail.com>
2018-10-27 19:58:57 +0500

    repo inter

--- src/main/java/ru/beginers/hackathon/repository/TicketRepository.java
+++ src/main/java/ru/beginers/hackathon/repository/TicketRepository.java
@@ -1,4 +1,19 @@
 package ru.beginers.hackathon.repository;
 
+import ru.beginers.hackathon.model.Ticket;
+
+import java.util.List;
+
 public interface TicketRepository {
+
+    Ticket save(Ticket Ticket, int userId);
+
+    boolean delete(int id, int userId);
+
+    Ticket get(int id, int userId);
+
+    // ORDERED dateTime desc
+    List<Ticket> getAll(int userId);
+
+    Ticket getWithUser(int id, int userId);
 }
--- src/main/java/ru/beginers/hackathon/repository/UserRepository.java
+++ src/main/java/ru/beginers/hackathon/repository/UserRepository.java
@@ -1,4 +1,23 @@
 package ru.beginers.hackathon.repository;
 
+import ru.beginers.hackathon.model.User;
+
+import java.util.List;
+
 public interface UserRepository {
+
+    User save(User user);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    User get(int id);
+
+    // null if not found
+    User getByLogin(String login);
+
+    List<User> getAll();
+
+    User getWithTickets(int id);
 }
